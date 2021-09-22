0434f4f3dcf6390de9a62b846bd027122b6e22bc
Cahyo Yunan Adianto <cahyoadianto10@gmail.com>
2021-09-22 13:41:15 +0700

    test

--- src/handler.js
+++ src/handler.js
@@ -83,7 +83,7 @@ const editNoteByIdHandler = (request, h) => {
     };
     const response = h.response({
       status: 'success',
-      message: 'Catatan berhasil diperbarui',
+      message: 'Catatan berhasil diperbarui!',
     });
     response.code(200);
     return response;
